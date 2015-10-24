c8c2f32015e55a12894f55270210b8174b0322de
Qu Ke <kequ@sohu-inc.com>
2015-10-24 13:38:55 +0800

    Initial Commit

---
+++ HeiMao_B/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeiMao_B
+//
+//  Created by kequ on 15/10/24.
+//  Copyright © 2015年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeiMao_B/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HeiMao_B
+//
+//  Created by kequ on 15/10/24.
+//  Copyright © 2015年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
