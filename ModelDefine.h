e0870cf9e72c14e152f309c6422ed31b9127f9d9
yjg2014 <18717357779@163.com>
2016-01-18 11:40:38 +0800

    修改预约状态

--- ModelDefine.h
+++ ModelDefine.h
@@ -14,58 +14,62 @@
 typedef NS_ENUM(NSInteger,KCourseStatue){
     
     KCourseStatueInvalid = 0,
-    KCourseStatueapplying = 1,   // 预约中(新订单)->1
-    KCourseStatueapplycancel = 2,// 学生取消（已取消）
-    KCourseStatueapplyconfirm,  // 已确定(新订单)
-    KCourseStatueapplyrefuse,      // 教练拒绝或者取消（已取消）
-    KCourseStatueunconfirmfinish, //  待确认完成------------无此状态
-    KCourseStatueucomments,    // 待评论(待评论)
-    KCourseStatueOnCommended, // 评论成功（已完成）
-    KCourseStatuefinish, // 订单完成（已完成）
-    KCourseStatuesystemcancel, // 系统取消（已取消）
-    KCourseStatuesignin, // 已签到(新订单)->3
-    KCourseStatuenosignin, // 未签到(已完成)
-
+    KCourseStatueapplying = 1,   // 预约中(新订单)-------"预约中"
+    KCourseStatueapplycancel = 2,// 学生取消（已取消)-------"学生取消"
+    KCourseStatueapplyconfirm,  // 已确定(新订单)-------"新订单"
+    KCourseStatueapplyrefuse,      // 教练拒绝或者取消（已取消）-------"已取消"
+    KCourseStatueunconfirmfinish, //  待确认完成(无此状态)-------"待确认完成"
+    KCourseStatueucomments,    // 待评论(待评论)-------"待评论"
+    KCourseStatueOnCommended, // 评论成功（已完成）-------"评论成功"
+    KCourseStatuefinish, // 订单完成（已完成）-------"订单完成"
+    KCourseStatuesystemcancel, // 系统取消（已取消）-------"系统取消"
+    KCourseStatuesignin, // 已签到(新订单)-------"已签到"
+    KCourseStatuenosignin, // 未签到(已完成)-------"已漏课"
+    
 };
-/*
-case  KCourseStatueInvalid :
-  break;
-case  KCourseStatueapplying :   // 预约中
-  return @"预约中";
-  break;
-case  KCourseStatueapplycancel :// 学生取消
-  return @"学生取消";
-  break;
-case  KCourseStatueapplyconfirm:  // 已确定
-  return @"新订单";
-  break;
-case  KCourseStatueapplyrefuse:      // 教练（拒绝或者取消）
-  return @"已取消";
-  break;
-case  KCourseStatueunconfirmfinish: //  待确认完成  (v1.1 中没有该字段)
-  return @"待确认完成";
-  break;
-case  KCourseStatueucomments:    // 待评论
-  return @"待评论";
-  break;
-case  KCourseStatueOnCommended: // 评论成功
-  return @"评论成功";
-  break;
-case  KCourseStatuefinish: // 订单完成
-  return @"订单完成";
-  break;
-case  KCourseStatuesystemcancel: // 系统取消
-  return @"系统取消";
-  break;
-case  KCourseStatuesignin: // 已签到
-  return @"已签到";
-  break;
-case  KCourseStatuenosignin: // 未签到
-  return @"未签到";
-*/
 
-//对应APP界面分类：
+
+#pragma mark -- 对应APP首页顶部分类
 //新订单:applyconfirm=3,  // 已确定
 //待评论:ucomments:6,//待评论
 //已取消:applycancel:2,// 学生取消
 //已完成:finish:7,  // 订单完成
+
+#pragma mark -- 对应APP界面显示
+/*
+ case  KCourseStatueInvalid :
+ break;
+ case  KCourseStatueapplying :   // 预约中
+ return @"预约中";
+ break;
+ case  KCourseStatueapplycancel :// 学生取消
+ return @"学生取消";
+ break;
+ case  KCourseStatueapplyconfirm:  // 已确定
+ return @"新订单";
+ break;
+ case  KCourseStatueapplyrefuse:      // 教练（拒绝或者取消）
+ return @"已取消";
+ break;
+ case  KCourseStatueunconfirmfinish: //  待确认完成  (v1.1 中没有该字段)
+ return @"待确认完成";
+ break;
+ case  KCourseStatueucomments:    // 待评论
+ return @"待评论";
+ break;
+ case  KCourseStatueOnCommended: // 评论成功
+ return @"评论成功";
+ break;
+ case  KCourseStatuefinish: // 订单完成
+ return @"订单完成";
+ break;
+ case  KCourseStatuesystemcancel: // 系统取消
+ return @"系统取消";
+ break;
+ case  KCourseStatuesignin: // 已签到
+ return @"已签到";
+ break;
+ case  KCourseStatuenosignin: // 未签到
+ return @"已漏课";
+ */
+
