f9bad23cacd9752d9f818267353a2439b0ff0c22
Mortime <Mortimey@163.com>
2016-01-20 16:45:07 +0800

    fix bug

--- HeiMao_B/PickerItemView.m
+++ HeiMao_B/PickerItemView.m
@@ -55,6 +55,7 @@
         _contentLabel.backgroundColor = [UIColor clearColor];
         _contentLabel.textAlignment = NSTextAlignmentLeft;
         _contentLabel.textColor = RGB_Color(0x33, 0x33, 0x33);
+        _contentLabel.userInteractionEnabled = YES;
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelDidClick)];
         [_contentLabel addGestureRecognizer:tap];
         [self addSubview:_contentLabel];
@@ -85,6 +86,7 @@
 }
 
 - (void)labelDidClick {
+    _seletedButton.selected = !_seletedButton.selected;
     [self buttonDidClick:_seletedButton];
 }
 
