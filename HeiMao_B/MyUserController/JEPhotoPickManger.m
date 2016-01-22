fcd611153e4bd5d7b3f79859c9d6fdf7103478b0
Mortime <Mortimey@163.com>
2016-01-22 14:43:11 +0800

    添加相机

--- HeiMao_B/MyUserController/JEPhotoPickManger.m
+++ HeiMao_B/MyUserController/JEPhotoPickManger.m
@@ -8,6 +8,7 @@
 
 #import "JEPhotoPickManger.h"
 #import "PFActionSheetView.h"
+#import <AVFoundation/AVFoundation.h>
 //#import "ToolHeader.h"
 @interface JEPhotoPickManger ()
 @property (weak, nonatomic) UIViewController<UINavigationControllerDelegate,UIImagePickerControllerDelegate> *fromVc;
@@ -20,22 +21,34 @@
     
     [PFActionSheetView showAlertWithTitle:nil message:nil cancelButtonTitle:@"取消" otherButtonTitles:@[@"拍照",@"从相册选取"] withVc:fromController completion:^(NSUInteger selectedOtherButtonIndex) {
         if (selectedOtherButtonIndex == 0) {
-            if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-                UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-                picker.allowsEditing = YES;
-                picker.delegate = fromController;
-                picker.sourceType = UIImagePickerControllerSourceTypeCamera;
-                picker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:picker.sourceType];
-                picker.navigationBar.barTintColor = fromController.navigationController.navigationBar.barTintColor;
-                picker.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor blackColor],
-                                                             NSFontAttributeName : [UIFont boldSystemFontOfSize:18]};
-
-                [fromController presentViewController:picker animated:YES completion:nil];
+            
+            
+            AVAuthorizationStatus state = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+            if (state == AVAuthorizationStatusAuthorized) {
+                
+                if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+                    
+                    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+                    picker.allowsEditing = YES;
+                    picker.delegate = fromController;
+                    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+                    picker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:picker.sourceType];
+                    picker.navigationBar.barTintColor = fromController.navigationController.navigationBar.barTintColor;
+                    picker.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor blackColor],
+                                                                 NSFontAttributeName : [UIFont boldSystemFontOfSize:18]};
+                    
+                    [fromController presentViewController:picker animated:YES completion:nil];
+                }
+ 
+                
             }else{
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"相机不可用" message:@"请在设置-隐私-相机 里面开启服务" delegate:self cancelButtonTitle:@"我知道了" otherButtonTitles:nil, nil];
+                
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"相机不可用" message:@"请在设置->一步教练->相机 里面开启服务" delegate:self cancelButtonTitle:@"我知道了" otherButtonTitles:nil, nil];
                 [alert show];
-
+                
             }
+
+            
         }else if (selectedOtherButtonIndex == 1) {
             if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
 
