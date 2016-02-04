f7a181191cf8e01be40b43a9d8737d72f76c2884
Mortime <Mortimey@163.com>
2016-02-04 19:54:58 +0800

    提交

--- HeiMao_B/OrderViewController/CourseViewController.m
+++ HeiMao_B/OrderViewController/CourseViewController.m
@@ -79,13 +79,13 @@
 
 - (void)viewDidLoad {
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        DVVStudentListController *listVC = [DVVStudentListController new];
-        [self.navigationController pushViewController:listVC animated:YES];
-        
-//        DVVSendMessageToStudentController *sendMsgVC = [DVVSendMessageToStudentController new];
-//        [self.navigationController pushViewController:sendMsgVC animated:YES];
-    });
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        DVVStudentListController *listVC = [DVVStudentListController new];
+//        [self.navigationController pushViewController:listVC animated:YES];
+//        
+////        DVVSendMessageToStudentController *sendMsgVC = [DVVSendMessageToStudentController new];
+////        [self.navigationController pushViewController:sendMsgVC animated:YES];
+//    });
     
     [super viewDidLoad];
    
