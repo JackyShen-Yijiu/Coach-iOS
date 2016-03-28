d1d6e7aeb58556c59605a161ff62f20fd3ada293
yjg2014 <18717357779@163.com>
2016-03-28 12:04:17 +0800

    教龄选择器控件添加

--- HeiMao_B/模块/我的/Controller/EditorUserViewController.m
+++ HeiMao_B/模块/我的/Controller/EditorUserViewController.m
@@ -38,9 +38,10 @@
 static NSString *const ktagArrChange = @"ktagArrChange";
 
 #define kDefaultTintColor   RGB_Color(0x28, 0x79, 0xF3)
+#define KpickViewH  200
 
 
-@interface EditorUserViewController ()<UITableViewDataSource,UITableViewDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+@interface EditorUserViewController ()<UITableViewDataSource,UITableViewDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate,UIPickerViewDataSource, UIPickerViewDelegate>
 @property (strong, nonatomic) UITableView *tableView;
 
 @property (strong, nonatomic) NSArray *dataArray;
@@ -63,6 +64,15 @@ static NSString *const ktagArrChange = @"ktagArrChange";
 
 @property (nonatomic, strong) UILabel *footerLabel;
 
+// 教龄选择器
+@property (nonatomic, strong) UIView *bgView;
+
+@property (nonatomic, strong) UIPickerView *pickerView;
+
+@property (nonatomic, strong) NSArray *teachAgeArray;
+
+@property (nonatomic, strong) NSString *resultAgeStr;
+
 @end
 
 @implementation EditorUserViewController
@@ -77,6 +87,23 @@ static NSString *const ktagArrChange = @"ktagArrChange";
         _imgArray = @[@[@""],@[@"name.png",@"sex",@"card",@"phone"],@[@"permit",@"age",@"work",@"teach",@"site"],@[@"explain"]];
     }
     return _imgArray;
+}
+- (NSArray *)teachAgeArray{
+    if (_teachAgeArray == nil) {
+        _teachAgeArray = @[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20",@"21",@"22",@"23",@"24",@"25",@"26",@"27",@"28",@"29",@"30",@"31",@"32",@"33",@"34",@"35",@"36",@"37",@"38",@"39",@"40"];
+    }
+    return _teachAgeArray;
+}
+- (UIView *)bgView{
+    if (_bgView == nil) {
+        _bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height - KpickViewH)];
+        _bgView.backgroundColor = [UIColor blackColor];
+        _bgView.alpha = 0.32;
+        _bgView.userInteractionEnabled = YES;
+        UITapGestureRecognizer *tapGe = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeView)];
+        [_bgView addGestureRecognizer:tapGe];
+    }
+    return _bgView;
 }
 - (NSMutableArray *)systemTagArray {
     if (!_systemTagArray) {
@@ -107,6 +134,16 @@ static NSString *const ktagArrChange = @"ktagArrChange";
     }
     return _footerLabel;
 }
+- (UIPickerView *)pickerView {
+    if (_pickerView == nil) {
+        _pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0 , self.view.height - KpickViewH, self.view.width, KpickViewH)];
+        _pickerView.delegate = self;
+        _pickerView.dataSource = self;
+        _pickerView.backgroundColor = [UIColor whiteColor];
+    }
+    return _pickerView;
+}
+
 - (NSArray *)detailDataArray {
     /*
      {
@@ -336,6 +373,15 @@ static NSString *const ktagArrChange = @"ktagArrChange";
          [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(trainGroundKey) name:ktrainGroundKey object:nil];
     
     // 个性说明在视图将要显示的时候进行重新加载数据,所以不需添加通知了
+}
+#pragma mark --- 手势方法
+- (void)removeView{
+    if ([self.resultAgeStr integerValue] != [UserInfoModel defaultUserInfo].Seniority) {
+        [self commintCoachAgeData];
+    }
+    [self.bgView removeFromSuperview];
+    [self.pickerView removeFromSuperview];
+    
 }
 - (void)startAddData{
     // 训练场地
@@ -575,8 +621,11 @@ static NSString *const ktagArrChange = @"ktagArrChange";
     }
     if (2 == indexPath.section && 1 == indexPath.row) {
         // 教龄
-        modifyjialinViewController *vc = [[modifyjialinViewController alloc] init];
-        [self.navigationController pushViewController:vc animated:YES];
+//        modifyjialinViewController *vc = [[modifyjialinViewController alloc] init];
+//        [self.navigationController pushViewController:vc animated:YES];
+        [self.view addSubview:self.pickerView];
+        [self.view addSubview:self.bgView];
+
         
     }
 //    if (2 == indexPath.section && 2 == indexPath.row) {
@@ -698,11 +747,68 @@ static NSString *const ktagArrChange = @"ktagArrChange";
     viewController.navigationItem.rightBarButtonItem = rightItem;
     
 }
+
+#pragma mark - pickerView data source and delegate
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.teachAgeArray.count;
+}
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.teachAgeArray[row];
+}
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSString *resultString = _teachAgeArray[row];
+    self.resultAgeStr = resultString;
+
+}
+
 - (void)clickCancel:(UIButton *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+#pragma mark -- 向服务器提交 教龄修改后的数据
+- (void)commintCoachAgeData {
+    
+    
+    NSString *updateUserInfoUrl = [NSString stringWithFormat:@"%@/%@",[NetWorkEntiry domain],kupdateUserInfo];
+    
+    NSDictionary *dicParam = @{@"Seniority":self.resultAgeStr,@"coachid":[UserInfoModel defaultUserInfo].userID};
+    
+    [JENetwoking startDownLoadWithUrl:updateUserInfoUrl postParam:dicParam WithMethod:JENetworkingRequestMethodPost withCompletion:^(id data) {
+        
+        if (!data) {
+            [self showTotasViewWithMes:@"网络连接错误，请稍后再试"];
+            return ;
+        }
+        
+        NSDictionary *dataParam = data;
+        NSNumber *messege = dataParam[@"type"];
+        NSString *msg = [NSString stringWithFormat:@"%@",dataParam[@"msg"]];
+        
+        if (messege.intValue == 1) {
+            
+            ToastAlertView *alerview = [[ToastAlertView alloc] initWithTitle:@"修改成功" controller:self];
+            [alerview show];
+            [UserInfoModel defaultUserInfo].Seniority = [self.resultAgeStr integerValue];
+            NSIndexPath *path = [NSIndexPath indexPathForRow:1 inSection:2];
+            [self.tableView reloadRowsAtIndexPaths:@[path] withRowAnimation:UITableViewRowAnimationNone];
+
+            
+        }else {
+            if(msg){
+                ToastAlertView *alerview = [[ToastAlertView alloc] initWithTitle:msg controller:self];
+                [alerview show];
+            }
+        }
+        
+    }];
 }
 
 @end
