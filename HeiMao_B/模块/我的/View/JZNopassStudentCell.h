11a3db3811b21d4286d4deceb3426d3fcb413f39
ManagerLK <leikai0391@163.com>
2016-04-01 21:09:00 +0800

    考试信息搭建

---
+++ HeiMao_B/模块/我的/View/JZNopassStudentCell.h
@@ -0,0 +1,23 @@
+//
+//  JZNopassStudentCell.h
+//  HeiMao_B
+//
+//  Created by 雷凯 on 16/4/1.
+//  Copyright © 2016年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZNopassStudentCell : UITableViewCell
+
+/// 未通过学生头像
+@property (nonatomic, strong) UIImageView *studentIcon;
+
+/// 未通过学生名字
+@property (nonatomic, strong) UILabel *studentName;
+
+/// 未通过学员姓名
+@property (nonatomic, strong) UILabel *StudentScore;
+
+
+@end
