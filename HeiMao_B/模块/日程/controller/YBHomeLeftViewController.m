1b9efebe2ca11a04292945e961339ad1cf166e05
yjg2014 <18717357779@163.com>
2016-04-06 10:30:17 +0800

    修改日程

--- HeiMao_B/模块/日程/controller/YBHomeLeftViewController.m
+++ HeiMao_B/模块/日程/controller/YBHomeLeftViewController.m
@@ -18,6 +18,7 @@
 #import "YBCourseTableView.h"
 #import "YBObjectTool.h"
 #import "JZCompletionConfirmationContriller.h"
+#import "NoContentTipView.h"
 
 @interface YBHomeLeftViewController ()<FDCalendarDelegate,YBFDCalendarDelegate,UIScrollViewDelegate>
 {
@@ -49,6 +50,8 @@
 @property (nonatomic,strong) UIScrollView *mainScrollView;
 
 @property (nonatomic,strong) UIButton *confimBtn;
+
+@property(nonatomic,strong)NoContentTipView * tipView1;
 
 @end
 
@@ -163,6 +166,11 @@
 //    [self.mainScrollView addSubview:self.leftTableView];
     [self.mainScrollView addSubview:self.centerCourseTableView];
 //    [self.mainScrollView addSubview:self.rightTableView];
+    
+    self.tipView1 = [[NoContentTipView alloc] initWithContetntTip:@"暂无数据"];
+    [self.tipView1 setHidden:YES];
+    [self.centerCourseTableView addSubview:self.tipView1];
+    self.tipView1.center = CGPointMake(self.centerCourseTableView.width/2.f, self.centerCourseTableView.height/2.f);
     
     self.headView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, self.view.width, calendarH)];
     self.headView.backgroundColor = [UIColor whiteColor];
@@ -435,6 +443,8 @@
             NSLog(@"ws.centerTableDataArray.count:%lu",(unsigned long)ws.centerTableDataArray.count);
             
             [ws.centerCourseTableView reloadData];
+
+            [self.tipView1 setHidden:ws.centerTableDataArray.count];
 
         }else{
             
