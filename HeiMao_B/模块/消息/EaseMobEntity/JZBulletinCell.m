4a83c50a6db176c46f13788fb99014ef7ea53055
ManagerLK <leikai0391@163.com>
2016-04-21 10:31:38 +0800

    pch文件添加全局UIScreen  size

---
+++ HeiMao_B/模块/消息/EaseMobEntity/JZBulletinCell.m
@@ -0,0 +1,24 @@
+//
+//  JZBulletinCell.m
+//  HeiMao_B
+//
+//  Created by 雷凯 on 16/4/21.
+//  Copyright © 2016年 ke. All rights reserved.
+//
+
+#import "JZBulletinCell.h"
+
+@implementation JZBulletinCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
